f02267d0662c4967c1243cf352d540a7b6a6c700
Denny Piza <dpiza@student.42sp.org.br>
2021-11-30 21:42:19 -0300

    fixes fork pathway

--- samples/run.c
+++ samples/run.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 17:48:57 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/30 21:32:47 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/30 21:40:15 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv, char **envp)
 	pid_t	pid;
 
 	pid = fork(); // fork retorna um pid para o processo filho, o programa pai continua no else
-	if (pid == 0)
+	if (pid > 0)
 	{
 		ret = execve(argv[1], argv + 1, envp); // o execve não procura o executável, então o argumento deve conter o caminho
 		if (ret < 0)
