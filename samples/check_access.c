b35239b9d46d73494d46ca42f5ce6f5ed2930900
Denny Piza <dpiza@student.42sp.org.br>
2021-11-30 21:36:07 -0300

    renames getenv to check_access and adds run

--- samples/check_access.c
+++ samples/check_access.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   getenv.c                                           :+:      :+:    :+:   */
+/*   check_access.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 13:51:18 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/30 15:02:04 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/30 21:31:32 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <bsd/bsd.h>
 
 void	check_file_access(const char *folder, const char *ent_name, int *ent_access)
 {
@@ -23,6 +24,7 @@ void	check_file_access(const char *folder, const char *ent_name, int *ent_access
 
 	bzero(full_path, 128);
 	strcat(full_path, folder);
+	strcat(full_path, "/");
 	strcat(full_path, ent_name);
 	// printf("%s", full_path);
 	ent_access[0] = access(full_path, R_OK); // identifica a permissão de leitura
@@ -31,25 +33,47 @@ void	check_file_access(const char *folder, const char *ent_name, int *ent_access
 	ent_access[3] = access(full_path, F_OK);
 }
 
+void	print_access(int *ent_access, int d_type, char *d_name)
+{
+	char	r;
+	char	w;
+	char	x;
+
+	if (strnstr(d_name, "..", 3) || strnstr(d_name, ".", 2) || ent_access[3])
+		return ;
+	r = '-';
+	w = '-';
+	x = '-';
+	if (ent_access[0] == 0)
+		r = 'r';
+	if (ent_access[1] == 0)
+		w = 'w';
+	if (ent_access[2] == 0)
+		x = 'x';
+	printf("%c%c%c", r, w, x);
+	printf(" %3i\t%s\n", d_type, d_name);
+}
+
 int	main(int c, char **v)
 {
-	char	*path;
 	struct dirent	*ret;
-	DIR	*diretorio;
-	int		ent_access[4];
+	DIR				*diretorio;
+	int				ent_access[4];
 
-	path = getenv("PATH"); // retorna a variável PATH do sistema nativo
-	printf("%s\n", path);
+	if (c < 2)
+		return (1);
 	diretorio = opendir(v[1]); // retorna um ponteiro pro primeiro item da pasta
+	if (!diretorio)
+		return (1);
 	while(ret = readdir(diretorio)) // itera cada item da pasta
 	{
 		if (ret == NULL)
 			break ;
 		check_file_access(v[1], ret->d_name, ent_access); // verifica a permissão de um item da pasta
-		printf("R:%i W:%i X:%i F:%i", ent_access[0], ent_access[1],
-						ent_access[2], ent_access[3]);
-		printf(" %i %s\n", ret->d_type, ret->d_name);
+		print_access(ent_access, ret->d_type, ret->d_name); // printa as permissões
 	}
 	closedir(diretorio); // fecha a pasta pra não dar leak
 	return (0);
 }
+
+/* compilar usando -lbsd */
---
+++ samples/run.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 17:48:57 by dpiza             #+#    #+#             */
+/*   Updated: 2021/11/30 21:32:47 by dpiza            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		ret;
+	int		status;
+	pid_t	pid;
+
+	pid = fork(); // fork retorna um pid para o processo filho, o programa pai continua no else
+	if (pid == 0)
+	{
+		ret = execve(argv[1], argv + 1, envp); // o execve não procura o executável, então o argumento deve conter o caminho
+		if (ret < 0)
+			printf("error\n"); // execve retorna -1 quando não consegue executar o programa
+		exit (0);
+	}
+	else
+		waitpid(pid, &status, WUNTRACED); // espera o processo filho encerrar
+	return (0);
+}
