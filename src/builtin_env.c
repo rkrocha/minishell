f02692f488d96345270eb823172bc83c43d85a38
rkrocha <rafaelkr7@gmail.com>
2021-12-04 10:54:22 -0300

    fixed bug that caused gaps to be created in env matrix

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:35:27 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/12/03 14:35:33 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/12/04 10:21:28 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	msh_env(t_shell *minishell, t_cmd *cmd)
 	while (i < ENV_SIZE) // é necessário iterar em tudo?
 	{
 		if (minishell->env[i])
-			ft_putendl(minishell->env[i]);
+			printf("%d %s\n", i, minishell->env[i]);
+			// ft_putendl(minishell->env[i]);
 		i++;
 	}
 	return (0);
--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 13:56:29 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/12/03 14:01:044 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/12/04 10:43:28 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 int	msh_unset(t_shell *minishell, t_cmd *cmd) // deve aceitar vários args
 {
-	int			var_count;
-	int			var_index;
+	int	i;
+	int	var_index;
 
 	(void)cmd; // arrumar depois de termos o parsing
 	var_index = search_env(minishell, "MAIL"); // tirar var hardcoded
 	if (var_index >= 0)
 	{
 		free(minishell->env[var_index]);
-		var_count = env_len(minishell);
-		minishell->env[var_index] = minishell->env[var_count];
-		minishell->env[var_count] = NULL;
+		i = var_index;
+		while (minishell->env[i + 1])
+		{
+			minishell->env[i] = minishell->env[i + 1];
+			i++;
+		}
+		minishell->env[i] = NULL;
 	}
 	return (0); // pode retornar >0 caso não consiga unsettar uma ou mais vars
 }
