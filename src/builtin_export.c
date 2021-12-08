c1ea41bbe9c95fff6b5732a5d3340016582cafd2
Denny Piza <dpiza@student.42sp.org.br>
2021-12-08 19:38:04 -0300

    adds a function to break the var_name

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,24 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 14:18:10 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/08 14:24:02 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/12/08 18:49:05 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*get_var_name(char *cmd)
+{
+	int	i;
+	char	*name;
+
+	i = 0;
+	while(cmd[i] != '=')
+		i++;
+	name = ft_substr(cmd, 0, i);
+	return (name);
+}
 
 int	msh_export(t_shell *minishell, t_cmd *cmd) // pode aceitar vários args
 {
 	int	var_actual;
 	int	var_pos;
 	int	var_last;
+	char	*var_name;
 
-	(void)cmd;
+	var_name = get_var_name(cmd->cmd_v[1]);  // quebra a string cmd_v
 	var_last = env_len(minishell);
-	var_pos = search_env(minishell, "MAIL"); // quebrar a string cdm_v
+	var_pos = search_env(minishell, var_name);
 	var_actual = var_last;
 	if (var_pos > -1)
 		var_actual = var_pos;
@@ -28,6 +41,7 @@ int	msh_export(t_shell *minishell, t_cmd *cmd) // pode aceitar vários args
 		return (-1);
 	if (var_actual == var_pos)
 		free(minishell->env[var_actual]);
-	minishell->env[var_actual] = ft_strdup("MAIL=1234");
+	free (var_name);
+	minishell->env[var_actual] = ft_strdup(cmd->cmd_v[1]);
 	return (0); // retonar >0 caso uma var não possa ser exportada
 }
