21202795045793c0e04bf5663e666719a5113bad
Denny Piza <dpiza@student.42sp.org.br>
2021-12-08 19:56:35 -0300

    adds tilde and minus as targets for cd

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,32 +6,27 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 13:27:02 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/08 19:36:51 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/08 19:52:36 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	msh_cd(t_shell *minishell, t_cmd *cmd)
+static void	update_env_pwd(t_shell *minishell)
 {
 	char	buffer[128];
 	t_cmd	*update_pwd;
 	t_cmd	*update_old_pwd;
 	char	*old_pwd_var;
 
-	cmd->return_value = chdir(cmd->cmd_v[1]);
-	if (cmd->return_value)
-		perror(strerror(errno));
 	old_pwd_var = get_env(minishell, "PWD");
-	
-	update_pwd = malloc(sizeof(t_cmd));
+	update_pwd = malloc(sizeof(t_cmd));						// quando o msh_export estiver pronto manda tudo em 1 comando só
 	ft_bzero(update_pwd, sizeof(t_cmd));
 	update_pwd->cmd_v = malloc(3 * sizeof(char *));
 	update_pwd->cmd_v[0] = ft_strdup("export");
 	update_pwd->cmd_v[1] = ft_strjoin("PWD=", getcwd(buffer, 128));
 	update_pwd->cmd_v[2] = NULL;
 	msh_export(minishell, update_pwd);
-
 	update_old_pwd = malloc(sizeof(t_cmd));					// quando o msh_export estiver pronto manda tudo em 1 comando só
 	ft_bzero(update_old_pwd, sizeof(t_cmd));
 	update_old_pwd->cmd_v = malloc(3 * sizeof(char *));
@@ -39,9 +34,26 @@ int	msh_cd(t_shell *minishell, t_cmd *cmd)
 	update_old_pwd->cmd_v[1] = ft_strjoin("OLDPWD=", old_pwd_var);
 	update_old_pwd->cmd_v[2] = NULL;
 	msh_export(minishell, update_old_pwd);
-	
 	free(old_pwd_var);
 	del_cmd(update_pwd);
 	del_cmd(update_old_pwd);
+}
+
+int	msh_cd(t_shell *minishell, t_cmd *cmd)
+{
+	char	*destiny;
+
+	if (ft_strncmp(cmd->cmd_v[1], "~", 2) == 0)
+		destiny = get_env(minishell, "HOME");
+	else if (ft_strncmp(cmd->cmd_v[1], "-", 2) == 0)
+		destiny = get_env(minishell, "OLDPWD");
+	else
+		destiny = ft_strdup(cmd->cmd_v[1]);
+	cmd->return_value = chdir(destiny);
+	if (cmd->return_value)
+		perror(strerror(errno));
+	else
+		update_env_pwd(minishell);
+	free(destiny);
 	return (cmd->return_value);
 }
