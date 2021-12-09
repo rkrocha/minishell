8d940d42dd77b3e2b179c8ed0ab2843e47bb80fd
Denny Piza <dpiza@student.42sp.org.br>
2021-12-09 20:57:38 -0300

    norm

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 10:53:48 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/12/09 15:16:21 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/09 20:57:23 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,6 @@ void	cmd_var_parser(t_shell *minishell, t_cmd *cmd);
 void	cmd_quotes_parser(t_cmd *cmd);
 
 char	*single_cmd_parser(t_shell *minishell, char	*str);
-
-
 
 void	del_cmd(void *elem);
 t_cmd	*get_cmd(void *elem);
