586ba0d364d5759bbab297737a09ac803415fbbe
Denny Piza <dpiza@student.42sp.org.br>
2021-12-08 22:55:10 -0300

    pwd now gets the PWD var value instead of running getcwd

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 14:55:15 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/08 15:02:53 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/08 22:52:40 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 int	msh_pwd(t_shell *minishell, t_cmd *cmd)
 {
-	char	buffer[128];
+	char	*pwd;
 
-	(void)minishell;
-	if (!getcwd(buffer, 128))
-		perror(strerror(errno));
-	else
-		ft_putendl(buffer);
+	pwd = get_env(minishell, "PWD");
+	ft_putendl(pwd);
+	free (pwd);
 	cmd->return_value = 0;
 	return (cmd->return_value);
 }
