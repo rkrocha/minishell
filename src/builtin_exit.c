3b44e9703f8b0ebde499ff741e6f1369289290bd
Denny Piza <dpiza@student.42sp.org.br>
2021-12-08 22:41:34 -0300

    adds argument verification and error message

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -3,21 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:30:17 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/08 12:58:09 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/12/08 22:37:08 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	numeric_argument(char *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i] && ft_isdigit(cmd[i]))
+		i++;
+	if (cmd[i] != '\0')
+		return (-1);
+	return (ft_atoi(cmd) % 256);
+}
+
 int	msh_exit(t_shell *minishell, t_cmd *cmd)
 {
-	if (cmd->cmd_v[1] && ft_atoi(cmd->cmd_v[1]))
-		cmd->return_value = ft_atoi(cmd->cmd_v[1]);
+	if (cmd->cmd_v[1])
+		cmd->return_value = numeric_argument(cmd->cmd_v[1]);
 	else
 		cmd->return_value = 0;
+	if (cmd->return_value == -1)
+	{
+		ft_putstr("minishell: exit: ");
+		ft_putstr(cmd->cmd_v[1]);
+		ft_putstr(": ");
+		ft_putendl("numeric argument required");
+		cmd->return_value = 2;
+	}
 	minishell->end = TRUE;
-	return (cmd->return_value); // futuramente tem que ser cmd->cmd_v[1], se existir; ft_atoi retorna 0 em caso de erro?
+	return (cmd->return_value);
 }
