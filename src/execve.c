2a7ba2577f5777559b1bdf28358fc4febb6ad77c
Denny Piza <dennypiza@hotmail.com>
2021-12-15 10:11:20 -0300

    adds check_dir to the cmd-argv

--- src/execve.c
+++ src/execve.c
@@ -6,37 +6,11 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 11:00:46 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/14 20:35:46 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/15 10:08:54 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*get_path(t_shell *minishell, t_cmd *cmd)
-{
-	char	*var_path;
-	char	**path_list;
-	char	*cmd_path;
-	int		i;
-
-	if (ft_strncmp(*cmd->argv, "./", 2) == 0)
-		return (ft_strdup(cmd->argv[0]));
-	var_path = get_env(minishell, "PATH");
-	path_list = ft_split(var_path, ':');
-	i = -1;
-	while (path_list[++i])
-	{
-		cmd_path = ft_strjoin(path_list[i], "/");
-		cmd_path = ft_strjoin_free(&cmd_path, cmd->argv[0]);
-		if (!access(cmd_path, F_OK & X_OK))
-			break ;
-		free(cmd_path);
-		cmd_path = NULL;
-	}
-	ft_split_free(&path_list);
-	free(var_path);
-	return (cmd_path);
-}
 
 static int	get_err(char *cmd, int err_n)
 {
@@ -59,6 +33,47 @@ static int	get_err(char *cmd, int err_n)
 		return (126);
 }
 
+static int	check_dir(char *cmd_argv)
+{
+	struct stat	buffer;
+	int			ret;
+
+	ret = stat(cmd_argv, &buffer);
+	printf("type/mode: %d\n", buffer.st_mode);
+	printf("ret: %d\n", ret);
+	return (ret);
+}
+
+static char	*get_path(t_shell *minishell, t_cmd *cmd)
+{
+	char	*var_path;
+	char	**path_list;
+	char	*cmd_path;
+	int		i;
+
+	if (ft_strncmp(*cmd->argv, "./", 2) == 0)
+	{
+		check_dir(cmd->argv[0]);
+		return (ft_strdup(cmd->argv[0]));
+	}
+	var_path = get_env(minishell, "PATH");
+	path_list = ft_split(var_path, ':');
+	i = -1;
+	while (path_list[++i])
+	{
+		cmd_path = ft_strjoin(path_list[i], "/");
+		cmd_path = ft_strjoin_free(&cmd_path, cmd->argv[0]);
+		if (!access(cmd_path, F_OK & X_OK))
+			break ;
+		free(cmd_path);
+		cmd_path = NULL;
+	}
+	ft_split_free(&path_list);
+	free(var_path);
+	if (!cmd_path)
+		cmd->return_value = get_err(cmd->argv[0], 0);
+	return (cmd_path);
+}
 
 int	msh_execve(t_shell *minishell, t_cmd *cmd)
 {
@@ -70,14 +85,12 @@ int	msh_execve(t_shell *minishell, t_cmd *cmd)
 
 	cmd_path = get_path(minishell, cmd);
 	if (!cmd_path)
-	{
-		cmd->return_value = get_err(cmd->argv[0], 0);
 		return (cmd->return_value);
-	}
-	if(pipe(fd))
-		ft_putendl(strerror(errno));
 	free(cmd->argv[0]);
 	cmd->argv[0] = cmd_path;
+	
+	if(pipe(fd))
+		ft_putendl(strerror(errno));
 	pid = fork();
 	if (pid == 0)
 	{
