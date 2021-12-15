9fd8c080214b811129ebbc8556257ce00327126f
Denny Piza <dennypiza@hotmail.com>
2021-12-15 10:31:57 -0300

    adds pipes to the party

--- src/router.c
+++ src/router.c
@@ -6,13 +6,22 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:53:06 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/12/14 12:22:38 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/14 13:35:01 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	cmd_exec(t_shell *minishell, t_cmd *current)
+static void	cmd_exec_single(t_shell *minishell, t_cmd *current)
+{
+	static int (*const	func_ptr[8])(t_shell *, t_cmd *) = {
+		msh_execve, msh_cd, msh_echo, msh_env,
+		msh_exit, msh_export, msh_pwd, msh_unset};
+
+	minishell->last_return = func_ptr[current->type](minishell, current);
+}
+
+static void	cmd_exec_pipes(t_shell *minishell, t_cmd *current)
 {
 	static int (*const	func_ptr[8])(t_shell *, t_cmd *) = {
 		msh_execve, msh_cd, msh_echo, msh_env,
@@ -24,17 +33,14 @@ static void	cmd_exec(t_shell *minishell, t_cmd *current)
 	exit(0);
 }
 
-void	cmd_router(t_shell *msh)
+static void	cmd_exec_pipes_iter(t_shell *msh, t_list *tracker)
 {
-	t_list	*tracker;
 	t_cmd	*current;
 	int		pid;
 	int		clone_in;
-
 	char	buffer[512];
-
+	
 	clone_in = dup(0);
-	tracker = msh->cmd_list;
 	pipe(msh->ret_fd);
 	while (tracker)
 	{
@@ -43,10 +49,13 @@ void	cmd_router(t_shell *msh)
 		pid = fork();
 		if (pid == 0)
 		{
+			// caso haja um redirect de entrada
+			// open("[]");
+			// caso haja um redirect de saída
 			dup2(msh->data_fd[1], 1);
 			close(msh->data_fd[0]);
 			close(msh->data_fd[1]);
-			cmd_exec(msh, current);
+			cmd_exec_pipes(msh, current);
 		}
 		wait(NULL);
 		read(msh->ret_fd[0], &msh->last_return, sizeof(msh->last_return));
@@ -62,6 +71,15 @@ void	cmd_router(t_shell *msh)
 	close(msh->ret_fd[0]);
 	close(msh->ret_fd[1]);
 	dup2(clone_in, 0);
-	// close(0);
-	// exit(0);
+}
+
+void	cmd_router(t_shell *minishell)
+{
+	t_list	*tracker;
+
+	tracker = minishell->cmd_list;
+	if (!tracker->next)
+		cmd_exec_single(minishell, ((t_cmd *)tracker->content));
+	else
+		cmd_exec_pipes_iter(minishell, tracker);
 }
