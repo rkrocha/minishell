c1210016bc8f6b06c47de0c80bd66df803c727af
rkrocha <rafaelkr7@gmail.com>
2021-12-23 15:13:58 -0300

    added ft_lstclear on cmd_list in function free_and_exit

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 14:29:06 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/12/23 11:45:57 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/12/23 13:39:56 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	free_and_exit(t_shell *minishell, int return_value)
 {
 	rl_clear_history();
 	ft_split_free(&minishell->env);
+	ft_lstclear(&minishell->cmd_list, del_cmd);
 	free(minishell->home);
 	free(minishell->pwd);
 	free(minishell->prompt);
