11de9f4bd0786bc3d89dc9078d7437d9271ecc50
Denny Piza <dennypiza@hotmail.com>
2021-12-27 16:13:19 -0300

    replaced gnl with readline

--- src/redirect.c
+++ src/redirect.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 13:39:28 by dpiza             #+#    #+#             */
-/*   Updated: 2021/12/23 18:56:59 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/12/27 15:11:49 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,12 @@ static void	sigexit(int signum)
 static int	here_doc(t_shell *msh, char *delim)
 {
 	char	*str;
-	char	*actual_delim;
+	// char	*actual_delim;
 	int		fildes[2];
 	int		status;
 	int		pid;
 
-	actual_delim = ft_strjoin(delim, "\n");
+	// actual_delim = ft_strjoin(delim, "\n");
 	pipe(fildes);
 	pid = fork();
 	if (pid == 0)
@@ -54,7 +54,7 @@ static int	here_doc(t_shell *msh, char *delim)
 		signal(SIGINT, sigexit);
 		while (1)
 		{
-			str = get_next_line(0);
+			str = readline("> ");
 			if (!str)
 			{
 				ft_putstr_fd("minishell: warning: here_document delimited by end-of-file (wanted `", 2);
@@ -62,11 +62,13 @@ static int	here_doc(t_shell *msh, char *delim)
 				ft_putendl_fd("`)", 2);
 				break ;
 			}
-			if (!ft_strncmp(str, actual_delim, ft_strlen(actual_delim)))
+			// if (!ft_strncmp(str, actual_delim, ft_strlen(actual_delim)))
+			if (!ft_strncmp(str, delim, ft_strlen(delim) + 1))
 			{
 				free(str);
 				break ;
 			}
+			str = ft_strjoin_free(&str, "\n");
 			write(fildes[1], str, ft_strlen(str));
 			free(str);
 		}
@@ -77,7 +79,7 @@ static int	here_doc(t_shell *msh, char *delim)
 	if (WEXITSTATUS(status) == 130)
 		fildes[0] = -1;
 	close(fildes[1]);
-	free(actual_delim);
+	// free(actual_delim);
 	return (fildes[0]);
 }
 
@@ -116,7 +118,7 @@ int	open_output(char *file_name, int type)
 	fd = -1;
 	if (!stat(file_name, &buffer) && buffer.st_mode == 16877)
 		throw_err(file_name, -2);
-	else if (access(file_name, F_OK) && !access(file_name, W_OK))
+	else if (!access(file_name, F_OK) && access(file_name, W_OK))
 		throw_err(file_name, -3);
 	if (type == 1)
 		fd = open(file_name, O_WRONLY | O_TRUNC | O_CREAT, 0644);
